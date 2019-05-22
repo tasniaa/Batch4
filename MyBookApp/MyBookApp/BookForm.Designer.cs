434c788537809ef3a6e3ff413d53b4584cc4b928
Tasniaa <mahajabintorosha@gmail.com>
2019-05-22 23:19:58 +0600

    BookForm_added

--- MyBookApp/MyBookApp/BookForm.Designer.cs
+++ MyBookApp/MyBookApp/BookForm.Designer.cs
@@ -28,61 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BookForm));
             this.ShowButton = new System.Windows.Forms.Button();
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.itemComboBox = new System.Windows.Forms.ComboBox();
+            this.orderComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.Quantity = new System.Windows.Forms.Label();
+            this.customerinformation = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // ShowButton
             // 
-            this.ShowButton.Location = new System.Drawing.Point(581, 193);
+            this.ShowButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ShowButton.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowButton.Location = new System.Drawing.Point(335, 331);
             this.ShowButton.Name = "ShowButton";
             this.ShowButton.Size = new System.Drawing.Size(75, 23);
             this.ShowButton.TabIndex = 0;
-            this.ShowButton.Text = "Show";
-            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Text = "Save";
+            this.ShowButton.UseVisualStyleBackColor = false;
             this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(164, 66);
+            this.nameTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.nameTextBox.Location = new System.Drawing.Point(233, 88);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.Size = new System.Drawing.Size(177, 20);
             this.nameTextBox.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 66);
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(93, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Customer Name";
             // 
-            // itemComboBox
+            // orderComboBox
             // 
-            this.itemComboBox.FormattingEnabled = true;
-            this.itemComboBox.Items.AddRange(new object[] {
-            "Bangla",
-            "English",
-            "Math"});
-            this.itemComboBox.Location = new System.Drawing.Point(168, 108);
-            this.itemComboBox.Name = "itemComboBox";
-            this.itemComboBox.Size = new System.Drawing.Size(121, 21);
-            this.itemComboBox.TabIndex = 3;
+            this.orderComboBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.orderComboBox.FormattingEnabled = true;
+            this.orderComboBox.Items.AddRange(new object[] {
+            "Bangla 90",
+            "English 100",
+            "Math 120",
+            "Art 80"});
+            this.orderComboBox.Location = new System.Drawing.Point(233, 215);
+            this.orderComboBox.Name = "orderComboBox";
+            this.orderComboBox.Size = new System.Drawing.Size(177, 21);
+            this.orderComboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contact No\r\n";
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.contactTextBox.Location = new System.Drawing.Point(233, 125);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(177, 20);
+            this.contactTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(109, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Address\r\n";
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.quantityTextBox.Location = new System.Drawing.Point(233, 265);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(177, 20);
+            this.quantityTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Order";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.addressTextBox.Location = new System.Drawing.Point(233, 168);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(177, 20);
+            this.addressTextBox.TabIndex = 8;
+            // 
+            // Quantity
+            // 
+            this.Quantity.AutoSize = true;
+            this.Quantity.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.Quantity.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quantity.Location = new System.Drawing.Point(109, 272);
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Size = new System.Drawing.Size(55, 13);
+            this.Quantity.TabIndex = 10;
+            this.Quantity.Text = "Quantity";
+            // 
+            // customerinformation
+            // 
+            this.customerinformation.AutoSize = true;
+            this.customerinformation.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.customerinformation.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerinformation.Location = new System.Drawing.Point(126, 16);
+            this.customerinformation.Name = "customerinformation";
+            this.customerinformation.Size = new System.Drawing.Size(127, 13);
+            this.customerinformation.TabIndex = 11;
+            this.customerinformation.Text = "Customer Information";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.customerinformation);
+            this.groupBox1.Location = new System.Drawing.Point(71, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 384);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, -35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(809, 473);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "                                "});
+            this.listBox1.Location = new System.Drawing.Point(458, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(318, 381);
+            this.listBox1.TabIndex = 16;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // BookForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.itemComboBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(this.orderComboBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "BookForm";
-            this.Text = "Book Store";
+            this.Text = "Book Shop";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,7 +235,18 @@
         private System.Windows.Forms.Button ShowButton;
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox itemComboBox;
+        private System.Windows.Forms.ComboBox orderComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Label Quantity;
+        private System.Windows.Forms.Label customerinformation;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- MyBookApp/MyBookApp/BookForm.cs
+++ MyBookApp/MyBookApp/BookForm.cs
@@ -12,6 +12,7 @@ namespace MyBookApp
 {
     public partial class BookForm : Form
     {
+       
         public BookForm()
         {
             InitializeComponent();
@@ -19,7 +20,21 @@ namespace MyBookApp
 
         private void ShowButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello "+ nameTextBox.Text+" " +itemComboBox.Text);
+            listBox1.Items.Add(  "                                           "+customerinformation.Text+ "\n");
+            listBox1.Items.Add("Customer Name : "+nameTextBox.Text);
+            listBox1.Items.Add("Contact No : "+contactTextBox.Text);
+            listBox1.Items.Add("Address : " + addressTextBox.Text);
+            listBox1.Items.Add("Order : "+orderComboBox.Text);
+            listBox1.Items.Add("Quantity : " + quantityTextBox.Text);
+
+            //MessageBox.Show("Customer Name: "+ nameTextBox.Text+ "\nContact No: " + contactTextBox.Text+"\nAddress: " + addressTextBox.Text+"\nOrder: " +orderComboBox.Text+"\nQuantity: "+ quantityTextBox.Text);
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+      
+
         }
     }
 }
