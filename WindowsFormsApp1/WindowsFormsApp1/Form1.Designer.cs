37222fd145b9ff2d8844762e0c335431dfccaeaa
Tasniaa <mahajabintorosha@gmail.com>
2019-05-28 06:48:56 +0600

    Form1_added

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.uniqueButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(287, 66);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 6;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(438, 64);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(287, 108);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(226, 279);
+            this.showRichTextBox.TabIndex = 4;
+            this.showRichTextBox.Text = "";
+            // 
+            // uniqueButton
+            // 
+            this.uniqueButton.Location = new System.Drawing.Point(578, 185);
+            this.uniqueButton.Name = "uniqueButton";
+            this.uniqueButton.Size = new System.Drawing.Size(75, 23);
+            this.uniqueButton.TabIndex = 12;
+            this.uniqueButton.Text = "Unique";
+            this.uniqueButton.UseVisualStyleBackColor = true;
+            this.uniqueButton.Click += new System.EventHandler(this.uniqueButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uniqueButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Button uniqueButton;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,71 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        const int size = 10;
+        int[] number = new int[size];
+        const int size2 = 12;
+        int[] number2 = new int[size2];
+        int index = 0;
+       
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private string Show()
+        {
+
+
+            string message = "";
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                    message = message + ("Element of index " + index + " is " + number[index].ToString() + "\n");
+
+
+            }
+            return message;
+        }
+        private void uniqueButton_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            string message = "";
+            message = "Array with Unique list  : ";
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = i + 1; j < size;)
+                {
+                    if (number[j] == number[i])
+                    {
+                        for (int k = j; k < size; k++)
+                        {
+                            number[k] = number[k -1];
+                        }
+                        i--;
+                    }
+                    else
+                        j++;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                message = message + number[i];
+            }
+
+            showRichTextBox.Text = message.ToString();
+            }
+        
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            index++;
+            number[index] = Convert.ToInt32(numberTextBox.Text);
+            showRichTextBox.Text = Show();
         }
     }
 }
